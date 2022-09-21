9b24ff4b5c9cbf3a84920ffde368321b1af56455
Huancutebaby05 <truongnv97.haui@gmail.com>
2022-09-21 21:21:22 +0900

    update 20220921

--- Case_study_2/css/main.js
+++ Case_study_2/css/main.js
@@ -1,16 +1,17 @@
-$(window).scroll(function () {
-    let otop = $("#product-menu").offset().top - window.innerHeight;
-    if ($(window).scrollTop() > otop) {
-        $("#header").addClass("stick");
-    } else {
-        $("#header").removeClass("stick");
-}   
-})
-$(window).scroll(function () {
-    let otop = $("#product-menu").offset().top - window.innerHeight;
-    if ($(window).scrollTop() > otop) {
-        $("help").addClass("help-visible");
-    } else {
-        $("help").removeClass("help-visible");
-}   
-})
+class Product{
+    image
+    title
+    price
+    add
+    like
+    constructor(image,title,price,desc){
+        this.image = image;
+        this.title = title;
+        this.price = price;
+        this.desc = desc
+    }
+
+}
+function render(){
+    
+}
